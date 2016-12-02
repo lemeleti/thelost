f033bca813350ac860c46889834c63c9bb76c522
lemeleti <leandromeleti97@hotmail.com>
2016-12-02 22:47:51 +0100

    prepare for development
    export custom codetemplates
    add dependency (JUnit)
    create the MainApp with generated comment

---
+++ TheLost/src/main/java/com/lmsoft/game/thelost/MainApp.java
@@ -0,0 +1,20 @@
+package com.lmsoft.game.thelost;
+
+/**
+ * Main class
+ * 
+ * @author C110426
+ * @date created on 02.12.2016
+ */
+public class MainApp {
+
+	/**
+	 * launch method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
