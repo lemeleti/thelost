3303c22d89ca72b0bc6330f754cad235efbea9d7
lemeleti <leandromeleti97@hotmail.com>
2016-12-08 19:36:33 +0100

    update view
    Update FXML and load it in the main

--- TheLost/src/main/java/com/lmsoft/game/thelost/MainApp.java
+++ TheLost/src/main/java/com/lmsoft/game/thelost/MainApp.java
@@ -1,6 +1,13 @@
 package com.lmsoft.game.thelost;
 
+import java.io.IOException;
+
+import com.lmsoft.game.thelost.controller.GameViewController;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -25,11 +32,28 @@ public class MainApp extends Application {
 	}
 
 	private void showMainView() {
-
+		showPlayView();
 	}
 
 	private void showPlayView() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/fxml/gameView.fxml"));
+			AnchorPane gameLayout = (AnchorPane) loader.load();
 
+			GameViewController controller = loader.getController();
+			controller.startGame();
+
+			Scene scene = new Scene(gameLayout);
+			primaryStage.setScene(scene);
+			primaryStage.setMinHeight(gameLayout.getPrefHeight());
+			primaryStage.setMinWidth(gameLayout.getPrefWidth());
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
